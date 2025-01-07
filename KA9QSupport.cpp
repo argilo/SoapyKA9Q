f5b7739cd58489aea8028431a93851acfdf726da
Clayton Smith <argilo@gmail.com>
2025-01-07 16:54:13 -0500

    Prototype

--- KA9QSupport.cpp
+++ KA9QSupport.cpp
@@ -1,14 +1,237 @@
 #include <SoapySDR/Device.hpp>
 #include <SoapySDR/Registry.hpp>
+#include <iostream>
+
+extern "C" {
+char const *App_path = "foo";
+#include "multicast.h"
+#include "status.h"
+}
 
 /***********************************************************************
  * Device interface
  **********************************************************************/
 class KA9Q : public SoapySDR::Device
 {
-    //Implement constructor with device specific arguments...
+private:
+    std::string m_status;
+    std::string m_data;
+    uint32_t m_ssrc;
 
-    //Implement all applicable virtual methods from SoapySDR::Device
+    double m_sampleRate = 0;
+    int m_Status_sock = -1;
+    int m_Control_sock = -1;
+    int m_Input_fd = -1;
+    uint8_t m_buffer[PKTSIZE];
+    uint8_t *m_dp = nullptr;
+    int m_bufferLen = 0;
+    int m_bufferOffset = 0;
+
+public:
+    KA9Q(const SoapySDR::Kwargs &args)
+    {
+        m_status = args.at("status");
+        m_data = args.at("data");
+        m_ssrc = std::stoi(args.at("ssrc"));
+
+        std::cout << m_status << " " << m_data << " " << m_ssrc << std::endl;
+    }
+
+    ~KA9Q() {}
+
+    std::string getDriverKey(void) const
+    {
+        return "KA9Q";
+    }
+
+    std::string getHardwareKey(void) const
+    {
+        return "KA9Q";
+    }
+
+    size_t getNumChannels(const int direction) const
+    {
+        return (direction == SOAPY_SDR_RX) ? 1 : 0;
+    }
+
+    bool getFullDuplex(const int direction, const size_t channel) const
+    {
+        (void)direction;
+        (void)channel;
+
+        return false;
+    }
+
+    void setSampleRate(const int direction, const size_t channel, const double rate)
+    {
+        (void)direction;
+        (void)channel;
+
+        m_sampleRate = rate;
+    }
+
+    double getSampleRate(const int direction, const size_t channel) const
+    {
+        (void)direction;
+        (void)channel;
+
+        return m_sampleRate;
+    }
+
+    SoapySDR::Stream *setupStream(
+        const int direction,
+        const std::string &format,
+        const std::vector<size_t> &channels = std::vector<size_t>(),
+        const SoapySDR::Kwargs &args = SoapySDR::Kwargs())
+    {
+        (void)direction;
+        (void)format;
+        (void)channels;
+        (void)args;
+
+        struct sockaddr_storage Control_address;
+        char iface[1024];
+        resolve_mcast(m_status.c_str(), &Control_address, DEFAULT_STAT_PORT, iface, sizeof(iface), 0);
+
+        m_Status_sock = listen_mcast(&Control_address, iface);
+        if (m_Status_sock == -1) {
+            // TODO
+        }
+
+        int Mcast_ttl = 1;
+        int IP_tos = 0;
+        m_Control_sock = connect_mcast(&Control_address, iface, Mcast_ttl, IP_tos);
+        if (m_Control_sock == -1) {
+            // TODO
+        }
+
+        m_Input_fd = setup_mcast_in(m_data.c_str(), NULL, 0, 0);
+        if (m_Input_fd == -1) {
+            // TODO
+        }
+
+        return (SoapySDR::Stream *) this;
+    }
+
+    void closeStream(SoapySDR::Stream *stream)
+    {
+        (void)stream;
+
+        m_Input_fd = -1;
+    }
+
+    int activateStream(
+        SoapySDR::Stream *stream,
+        const int flags = 0,
+        const long long timeNs = 0,
+        const size_t numElems = 0)
+    {
+        (void)stream;
+        (void)flags;
+        (void)timeNs;
+        (void)numElems;
+
+        return 0;
+    }
+
+    int deactivateStream(
+        SoapySDR::Stream *stream,
+        const int flags = 0,
+        const long long timeNs = 0)
+    {
+        (void)stream;
+        (void)flags;
+        (void)timeNs;
+
+        return 0;
+    }
+
+    int readStream(
+        SoapySDR::Stream *stream,
+        void * const *buffs,
+        const size_t numElems,
+        int &flags,
+        long long &timeNs,
+        const long timeoutUs = 100000)
+    {
+        (void)stream;
+        (void)flags;
+        (void)timeNs;
+        (void)timeoutUs;
+
+        float *out = (float *) buffs[0];
+        size_t outOffset = 0;
+
+        struct sockaddr sender;
+        socklen_t socksize = sizeof(sender);
+
+        while (outOffset < numElems * 2) {
+            if (m_bufferOffset < m_bufferLen) {
+                int16_t samp = (m_dp[2*m_bufferOffset] << 8) | m_dp[2*m_bufferOffset + 1];
+                out[outOffset++] = samp / 32768.0;
+                m_bufferOffset++;
+            } else {
+                int size = recvfrom(m_Input_fd, m_buffer, sizeof(m_buffer), 0, &sender, &socksize);
+                if (size == -1) {
+                    // TODO
+                    continue;
+                }
+
+                if (size < RTP_MIN_SIZE) {
+                    // TODO
+                    continue;
+                }
+
+                struct rtp_header rtp;
+                m_dp = (uint8_t *) ntoh_rtp(&rtp, m_buffer);
+
+                size -= m_dp - m_buffer;
+                if (rtp.pad) {
+                    // Remove padding
+                    size -= m_dp[size-1];
+                    rtp.pad = 0;
+                }
+                if(size <= 0) {
+                    // TODO
+                    continue;
+                }
+
+                if(rtp.ssrc != m_ssrc) {
+                    // TODO
+                    continue;
+                }
+
+                m_bufferLen = size / 2;
+                m_bufferOffset = 0;
+            }
+        }
+        return numElems;
+    }
+
+    void setFrequency(
+        const int direction,
+        const size_t channel,
+        const double frequency,
+        const SoapySDR::Kwargs &args = SoapySDR::Kwargs())
+    {
+        (void)direction;
+        (void)channel;
+        (void)args;
+
+        uint8_t cmd_buffer[PKTSIZE];
+        uint8_t *bp = cmd_buffer;
+        *bp++ = 1; // Generate command packet
+        uint32_t sent_tag = arc4random();
+        encode_int(&bp, COMMAND_TAG, sent_tag);
+        encode_int(&bp, OUTPUT_SSRC, m_ssrc);
+    	encode_double(&bp, RADIO_FREQUENCY, frequency); // Hz
+        encode_eol(&bp);
+
+        int cmd_len = bp - cmd_buffer;
+        if (send(m_Control_sock, cmd_buffer, cmd_len, 0) != cmd_len) {
+	        // TODO
+        }
+    }
 };
 
 /***********************************************************************
@@ -17,10 +240,18 @@ class KA9Q : public SoapySDR::Device
 SoapySDR::KwargsList findKA9Q(const SoapySDR::Kwargs &args)
 {
     (void)args;
-    //locate the device on the system...
-    //return a list of 0, 1, or more argument maps that each identify a device
 
-    return SoapySDR::KwargsList();
+    SoapySDR::KwargsList results;
+
+    SoapySDR::Kwargs devInfo;
+    devInfo["label"] = "KA9Q-Radio (hf.local, foobar.local, 1234)";
+    devInfo["status"] = "hf.local";
+    devInfo["data"] = "foobar.local";
+    devInfo["ssrc"] = "1234";
+
+    results.push_back(devInfo);
+
+    return results;
 }
 
 /***********************************************************************
@@ -28,10 +259,7 @@ SoapySDR::KwargsList findKA9Q(const SoapySDR::Kwargs &args)
  **********************************************************************/
 SoapySDR::Device *makeKA9Q(const SoapySDR::Kwargs &args)
 {
-    (void)args;
-    //create an instance of the device object given the args
-    //here we will translate args into something used in the constructor
-    return new KA9Q();
+    return new KA9Q(args);
 }
 
 /***********************************************************************
---
+++ multicast.h
@@ -0,0 +1,224 @@
+// Multicast and RTP functions, constants and structures for ka9q-radio
+// Copyright 2018-2023, Phil Karn, KA9Q
+
+#ifndef _MULTICAST_H
+#define _MULTICAST_H 1
+#include <stdint.h>
+#include <stdbool.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <assert.h>
+
+#define DEFAULT_MCAST_PORT (5004)
+#define DEFAULT_RTP_PORT (5004)
+#define DEFAULT_RTCP_PORT (5005)
+#define DEFAULT_STAT_PORT (5006)
+
+#define NTP_EPOCH 2208988800UL // Seconds between Jan 1 1900 and Jan 1 1970
+
+#define RTP_MIN_SIZE 12  // min size of RTP header
+#define RTP_VERS 2U
+#define RTP_MARKER 0x80  // Marker flag in mpt field
+
+extern int Mcast_ttl;
+extern int IP_tos;
+
+enum encoding {
+  NO_ENCODING = 0,
+  S16LE,
+  S16BE,
+  OPUS,
+  F32LE,
+  AX25,
+  F16LE,
+  UNUSED_ENCODING, // Sentinel, not used
+};
+
+struct pt_table {
+  unsigned int samprate;
+  unsigned int channels;
+  enum encoding encoding;
+};
+
+extern struct pt_table PT_table[];
+extern int const Opus_pt; // Allow dynamic setting in the future
+extern int const AX25_pt;
+
+// Internal representation of RTP header -- NOT what's on wire!
+struct rtp_header {
+  int version;
+  uint8_t type;
+  uint16_t seq;
+  uint32_t timestamp;
+  uint32_t ssrc;
+  bool marker:1;
+  bool pad:1;
+  bool extension:1;
+  int cc;
+  uint32_t csrc[15];
+};
+
+// RTP sender/receiver state
+struct rtp_state {
+  uint32_t ssrc;
+  uint8_t type;
+  bool init;
+  uint16_t seq;
+  uint16_t odd_seq;
+  bool odd_seq_set;
+  uint32_t timestamp;
+  uint64_t packets;
+  uint64_t bytes;
+  uint64_t drops;
+  uint64_t dupes;
+};
+
+// Internal format of sender report segment
+struct rtcp_sr {
+  uint32_t ssrc;
+  int64_t ntp_timestamp;
+  unsigned int rtp_timestamp;
+  unsigned int packet_count;
+  unsigned int byte_count;
+};
+
+// Internal format of receiver report segment
+struct rtcp_rr {
+  uint32_t ssrc;
+  int lost_fract;
+  int lost_packets;
+  int highest_seq;
+  int jitter;
+  int lsr; // Last SR
+  int dlsr; // Delay since last SR
+};
+
+// Internal format of RTCP source description
+enum sdes_type {
+  CNAME=1,
+  NAME=2,
+  EMAIL=3,
+  PHONE=4,
+  LOC=5,
+  TOOL=6,
+  NOTE=7,
+  PRIV=8,
+};
+
+// Individual source description item
+struct rtcp_sdes {
+  enum sdes_type type;
+  uint32_t ssrc;
+  int mlen;
+  char message[256];
+};
+
+char const *formatsock(void const *,bool);
+char *formataddr(char *result,int size,void const *s);
+
+#define PKTSIZE 65536 // Largest possible IP datagram, in case we use jumbograms
+// Incoming RTP packets
+// This should probably be extracted into a more general RTP library
+struct packet {
+  struct packet *next;
+  struct rtp_header rtp;
+  uint8_t const *data; // Don't modify a packet through this pointer
+  size_t len;
+  uint8_t content[PKTSIZE];
+};
+
+
+
+// Convert between internal and wire representations of RTP header
+void const *ntoh_rtp(struct rtp_header *,void const *);
+void *hton_rtp(void *, struct rtp_header const *);
+
+extern char const *Default_mcast_iface;
+
+int setup_mcast(char const *target,struct sockaddr *,int output,int ttl,int tos,int offset,int tries);
+static inline int setup_mcast_in(char const *target,struct sockaddr *sock,int offset,int tries){
+  return setup_mcast(target,sock,0,0,0,offset,tries);
+}
+int join_group(int fd,struct sockaddr const * const sock, char const * const iface,int const ttl,int const tos);
+int connect_mcast(void const *sock,char const *iface,int const ttl,int const tos);
+int listen_mcast(void const *sock,char const *iface);
+int resolve_mcast(char const *target,void *sock,int default_port,char *iface,int iface_len,int tries);
+int setportnumber(void *sock,uint16_t port);
+int getportnumber(void const *sock);
+int address_match(void const *arg1,void const *arg2);
+int add_pt(int type, unsigned int samprate, unsigned int channels, enum encoding encoding);
+
+// Function to process incoming RTP packet headers
+// Returns number of samples dropped or skipped by silence suppression, if any
+int rtp_process(struct rtp_state *state,struct rtp_header const *rtp,int samples);
+
+// Generate RTCP source description segment
+uint8_t *gen_sdes(uint8_t *output,int bufsize,uint32_t ssrc,struct rtcp_sdes const *sdes,int sc);
+// Generate RTCP bye segment
+uint8_t *gen_bye(uint8_t *output,int bufsize,uint32_t const *ssrcs,int sc);
+// Generate RTCP sender report segment
+uint8_t *gen_sr(uint8_t *output,int bufsize,struct rtcp_sr const *sr,struct rtcp_rr const *rr,int rc);
+// Generate RTCP receiver report segment
+uint8_t *gen_rr(uint8_t *output,int bufsize,uint32_t ssrc,struct rtcp_rr const *rr,int rc);
+
+void dump_interfaces(void);
+
+// Utility routines for reading from, and writing integers to, network format in char buffers
+static inline uint8_t get8(uint8_t const *dp){
+  assert(dp != NULL);
+  return *dp;
+}
+
+static inline uint16_t get16(uint8_t const *dp){
+  assert(dp != NULL);
+  return dp[0] << 8 | dp[1];
+}
+
+static inline uint32_t get24(uint8_t const *dp){
+  assert(dp != NULL);
+  return dp[0] << 16 | dp[1] << 8 | dp[2];
+}
+
+static inline uint32_t get32(uint8_t const *dp){
+  assert(dp != NULL);
+  return dp[0] << 24 | dp[1] << 16 | dp[2] << 8 | dp[3];
+}
+
+static inline uint8_t *put8(uint8_t *dp,uint8_t x){
+  assert(dp != NULL);
+  *dp++ = x;
+  return dp;
+}
+
+static inline uint8_t *put16(uint8_t *dp,uint16_t x){
+  assert(dp != NULL);
+  *dp++ = x >> 8;
+  *dp++ = x;
+  return dp;
+}
+
+static inline uint8_t *put24(uint8_t *dp,uint32_t x){
+  assert(dp != NULL);
+  *dp++ = x >> 16;
+  *dp++ = x >> 8;
+  *dp++ = x;
+  return dp;
+}
+
+static inline uint8_t *put32(uint8_t *dp,uint32_t x){
+  assert(dp != NULL);
+  *dp++ = x >> 24;
+  *dp++ = x >> 16;
+  *dp++ = x >> 8;
+  *dp++ = x;
+  return dp;
+}
+int samprate_from_pt(int type);
+int channels_from_pt(int type);
+enum encoding encoding_from_pt(int type);
+int pt_from_info(unsigned int samprate,unsigned int channels,enum encoding);
+char const *encoding_string(enum encoding);
+enum encoding parse_encoding(char const *str);
+uint32_t make_maddr(char const *arg);
+
+#endif
